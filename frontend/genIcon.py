7ba1621e6fb5b3fa48e31fac2f68b922409e595d
Ty Veldhouse <tpv8@calvin.edu>
2026-03-20 22:02:43 -0400

    quick fixing of parameters

--- frontend/genIcon.py
+++ frontend/genIcon.py
@@ -7,10 +7,10 @@ from nicegui import ui
 import genVisbilityCircle
 
 def genIcon (xcoord, ycoord, scalar, radVisible, satelliteImage) :
-    m = ui.leaflet(center=(52.5165, 13.4047), zoom=13)
+    m = ui.leaflet(center=(xcoord, ycoord), zoom=13)
     m.image_overlay(
         url = satelliteImage,
         bounds = [[xcoord + scalar, ycoord + scalar], [xcoord + scalar, ycoord + scalar]],
         options = {'opacity': 0.8},
-        genVisibilityCircle.visCircle(xcoord, ycoord, radVis)
+        genVisbilityCircle.visCircle(xcoord, ycoord, radVisible)
     )
