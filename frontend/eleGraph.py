b9264bc15d1e47bc4d4454087896f15655e170f2
Ty Veldhouse <tpv8@calvin.edu>
2026-03-21 12:09:07 -0400

    update_graph added

--- frontend/eleGraph.py
+++ frontend/eleGraph.py
@@ -2,21 +2,35 @@
 #
 # written by Ty Veldhouse (tpv8@calvin.edu) for Calvin Hackathon '26 on March 2026
 
-def latlon_to_cart(lat, lon, earthRad):
-    # Goal: Calculate the satellite's Cartesian Coordinates from Latitude and Longitude
-    # Parameters: Satellite Latitude & Longitude and Radius of the Earth
+import numpy as np
+from nicegui import ui
 
-    # Convert lat & lon to radians
-    lat = math.radians(lat)
-    lon = math.radians(lon)
-
-    # x y z coordinate calculations
-    x = earthRad * math.cos(lat) * math.cos(lon)
-    y = earthRad * math.cos(lat) * math.sin(lon)
-    z = earthRad * math.sin(lat)
-
-    return (x, y, z)
-
-def update_graph (user_lat, user_lon, sat_lat, sat_lon, alt) :
+def update_graph (time_alt) :
     # Goal: Update/Draw the graph of the visibility of a selected Satellite
-    # Parameters: User Latitude & Longitude, Satellite Latitude & Longitude, Altitude
+    # Parameters: time_alt - List of tuples (Time, Altitude)
+
+    # Position of graph
+    with ui.row().classes('w-full justify-center'):
+
+        # Drawing Graph
+        with ui.matplotlib(figsize=(3, 2)).figure as fig:
+
+            # Creation and naming of axes
+            axes = fig.gca()
+            axes.set_title('Satellite Elevation Over Time')
+            axes.set_xlabel('Time')
+            axes.set_ylabel('Elevation (degrees)')
+
+            # Unzipping Tuples
+            times = []
+            alts = []
+
+            for i in range(len(time_alt)):
+                times.append(int(print(time_alt[i][0])))
+                alts.append(time_alt[i][1])
+
+            # Plots on the map
+            axes.plot(times, alts, '-')
+
+# Needed?
+# ui.run()
