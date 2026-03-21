5367204d1bbf3a3bc651089d5c02b4cf738eae6f
isaiahvh7 <isaiahvanhorn7@gmail.com>
2026-03-21 02:42:27 -0400

    aa

--- frontend/drawSatellite.py
+++ frontend/drawSatellite.py
@@ -13,26 +13,49 @@ def drawVisCircle(my_map, lat, lon, radVis) :
         name='circle', 
         args=[[lat, lon], {'color': 'red', 'radius': radVis}]
     )
-
-def drawFlightPath(my_map, coords) :
+    
+def drawFlightPath(my_map, coords, line_color):
     # Goal: To draw the pre-calculated future flightpath the satellite using a polyline
-    # Parameters: coords - a List of tuples [lat, lon]
-    my_map.generic_layer(
-    name = 'polyline', 
-    args = [coords, {'color': 'red', 'weight': 4, 'opacity': 0.8}]
-    )
+    # Parameters: coords - a List of tuples/lists [lat, lon]
+    
+    segments = []
+    current_segment = []
 
-def drawSatellite (my_map, lat, lon, radVisible, coords,
+    for i, coord in enumerate(coords):
+        if i > 0:
+            prev_lon = coords[i-1][1]
+            curr_lon = coord[1]
+            
+            # If the jump is larger than 180 degrees, we crossed the map edge!
+            if abs(curr_lon - prev_lon) > 180:
+                segments.append(current_segment) # Save the line we were drawing
+                current_segment = []             # Start a new line
+                
+        current_segment.append(coord)
+        
+    # Append the final segment after the loop finishes
+    if current_segment:
+        segments.append(current_segment)
+
+    # Draw each segment as its own separate polyline
+    for segment in segments:
+        if len(segment) > 1: # Only draw if there are enough points to make a line
+            my_map.generic_layer(
+                name='polyline', 
+                args=[segment, {'color': 'red', 'weight': 4, 'opacity': 0.8}]
+            )
+
+def drawSatellite (my_map, lat, lon, radVisible, coords, line_color,
              imageURL= "https://img.freepik.com/premium-psd/satellite-isolated-transparent-background_1073071-13672.jpg") :
     # Goal: Generate map marker object and return it for future changes
     # Parameters: Latitude, Longitude, Radian of Visible Area Currently, Flightpath Coordinates, Icon Image URL
     # Returns: Marker object
     # Calls: Current Visibility Circle around itself
 
-    marker = my_map.marker(latlng = (lat, lon)) # initialize marker
+    marker = my_map.marker(latlng = (lat, lon))#.tooltip(name) # initialize marker
     # custom marker icon javascript
     custom_icon_js = '''L.icon({
-    iconUrl: imageURL,
+    iconUrl: https://img.freepik.com/premium-psd/satellite-isolated-transparent-background_1073071-13672.jpg,
     iconSize: [40, 40], // width and height of the icon
     iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
     })'''
@@ -40,5 +63,5 @@ def drawSatellite (my_map, lat, lon, radVisible, coords,
     marker.run_method(':setIcon', custom_icon_js)
 
     drawVisCircle(my_map, lat, lon, radVisible)
-    drawFlightPath(my_map, coords)
+    drawFlightPath(my_map, coords, line_color)
     return marker
